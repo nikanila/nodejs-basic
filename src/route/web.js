91185a40d71df78f2214774121eb50712c59f6fe
Hona <nguyenmanhhoan2k4@gmail.com>
2025-04-22 00:21:46 +0700

    finish CRUD

--- src/route/web.js
+++ src/route/web.js
@@ -5,9 +5,11 @@ let router = express.Router();
 
 const initWebRoute = (app) => {
     router.get("/", homeController.getHomePage);
-    router.get('/detail/user/:id', homeController.getDetailPage)
-    router.post('/create-new-user', homeController.createNewUser)
-    router.get('/about', aboutController.getAboutPage);
+    router.get('/detail/user/:id', homeController.getDetailPage);
+    router.post('/create-new-user', homeController.createNewUser);
+    router.post('/delete-user', homeController.deleteUser);
+    router.get('/edit-user/:id', homeController.getEditPage);
+    router.post('/update-user', homeController.postUpdateUser);
 
     return app.use('/', router);
 }
